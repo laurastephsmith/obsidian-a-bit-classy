61a4c23ee4f35eb95f5edad1b120837a66e20b25
Laura Smith <laurastephsmith@users.noreply.github.com>
2022-05-12 01:32:11 +0100

    Gulp to copy CSS to Laura's Obsidian theme dir

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 // Sass configuration
 var gulp = require('gulp');
 var sass = require('gulp-sass')(require('sass'));
+var rename = require('gulp-rename');
 
 gulp.task('sass', function(cb) {
   gulp
     .src('src/*.scss')
     .pipe(sass())
-    .pipe(
-      gulp.dest('.')
-    );
+    .pipe(gulp.dest('.'))
+    .pipe(rename('A Bit Classy.css'))
+    .pipe(gulp.dest('/home/laura/Dropbox/Library/.obsidian/themes/'));
   cb();
 });
 
