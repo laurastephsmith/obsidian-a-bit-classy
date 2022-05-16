281465d623b076938d34de3d778ee1587ce5afbf
Laura Smith <laurastephsmith@users.noreply.github.com>
2022-05-17 00:44:04 +0100

    Style: fix problems from linter

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 npx stylelint --fix "**/*.scss"
 
 # Compile Sass
-sass src/obsidian.scss obsidian.css
+npx sass src/obsidian.scss obsidian.css
 
 # Copy CSS into Obsidian themes directory
 cp obsidian.css $OBSIDIAN_VAULT_PATH/.obsidian/themes/A\ Bit\ Classy.css
